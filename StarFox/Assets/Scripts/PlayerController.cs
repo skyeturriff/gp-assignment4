158d954b78c19d263a3afa368d4d5875aa70bcb0
skyeturriff <turriff.skye@gmail.com>
2016-04-24 17:14:43 -0400

    Add menu music, button sounds, set up game scene environment, start player

---
+++ StarFox/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+    public float flySpeed;
+
+	void Start () 
+    {
+	
+	}
+	
+	void Update () 
+    {
+        transform.position += Vector3.forward * flySpeed * Time.deltaTime;
+	}
+}
--- StarFox/Assets/Scripts/SwitchScene.cs
+++ StarFox/Assets/Scripts/SwitchScene.cs
@@ -2,11 +2,31 @@
 using System.Collections;
 using UnityEngine.SceneManagement;
 
+/**
+ * This script plays an audio clip attached to the GameObject's AudioSource
+ * component to its full length before loading the scene specified by sceneName.
+ * Used to play a button's on-click sound effect before loading a scene.
+ */
 public class SwitchScene : MonoBehaviour 
 {
+    AudioSource audioSource;    // Sound to play on button click
+
+    void Start() {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     // Load the scene with the specified sceneName
     public void LoadSceneByName(string sceneName)
     {
+        StartCoroutine(DelayLoad(sceneName));       
+    }
+
+    // Coroutine allows the button's click sound to play fully before
+    // the new scene is loaded and the AudioSource inaccessible
+    IEnumerator DelayLoad(string sceneName)
+    {
+        audioSource.Play();
+        yield return new WaitForSeconds(audioSource.clip.length);
         SceneManager.LoadScene(sceneName);
     }
 }
