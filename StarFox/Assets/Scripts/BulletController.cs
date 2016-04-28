9763b878dff662c8dc9169d0c5c206f35bf70530
skyeturriff <turriff.skye@gmail.com>
2016-04-27 21:54:55 -0400

    Add player movements and shooting

---
+++ StarFox/Assets/Scripts/BulletController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * This script controls behaviour of bullets (projectiles). Causes bullets 
+ * to be propelled forward in their current facing direction, and to "die" 
+ * after lifespan seconds have passed since instantiation.
+ */
+public class BulletController : MonoBehaviour {
+
+    Rigidbody rb;
+    float movementSpeed;    // Speed at which the bullet travels
+    float timeAlive;        // Time bullet was instantiated
+    float lifespan;         // Length of time bullet can be active for
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        movementSpeed = 5.0f;
+        lifespan = 0.5f;
+        timeAlive = Time.time;
+    }
+
+    // Calculate how long bullet has been active for, and destroy
+    // bullet if it is longer than the allowed lifespan
+    void Update()
+    {
+        float timeSinceAlive = Time.time - timeAlive;
+
+        if (timeSinceAlive >= lifespan)
+            DestroyObject(gameObject);
+    }
+
+    // Propell bullet forward in its current direction
+    void FixedUpdate()
+    {
+        rb.AddForce(-transform.forward * movementSpeed);
+    }
+}
--- StarFox/Assets/Scripts/PlayerController.cs
+++ StarFox/Assets/Scripts/PlayerController.cs
@@ -3,15 +3,74 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour 
 {
-    public float flySpeed;
+    Rigidbody rb;                       // Allow physics actions to be applied to player
+    public GameObject bullet;           // Reference to bullet prefab player will fire
+
+    public float flySpeed;              // Controls magnitude of force applied to player in beginning
+    public float rollSpeed;             // Speed to complete aileron loop
+    private Vector3 rotation;           // Stores player GameObject's rotation
+    private Vector3 rollDirection;      // Direction to perform aileron loop in
 
 	void Start () 
     {
-	
+        // Apply initial force to propel GameObject forward continuously
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(Vector3.forward * flySpeed);
+
+        // Get start positioning of player GameObject
+        rotation = transform.rotation.eulerAngles;
+        rollDirection = rotation;
 	}
 	
 	void Update () 
     {
-        transform.position += Vector3.forward * flySpeed * Time.deltaTime;
+        // Check for fire
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject spawnedBullet = GameObject.Instantiate(bullet);
+            spawnedBullet.transform.position = new Vector3(
+                transform.position.x, transform.position.y, transform.position.z + 1.0f);
+            spawnedBullet.transform.rotation = transform.rotation;
+        }
+
+        // Check for horizontal movement (aileron roll)
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            rollDirection.z += 360.0f;
+        }
+
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            rollDirection.z -= 360.0f;
+        }
+
+        // Perform aileron roll using change in rotation per second
+        rotation = Vector3.Lerp(rotation, rollDirection, rollSpeed * Time.deltaTime);
+        transform.eulerAngles = rotation;      
 	}
+
+    void FixedUpdate()
+    {
+        // Check for change in elevation of player (inverted)
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.AddForce(Vector3.down);
+        }
+
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rb.AddForce(Vector3.up);
+        }
+
+        // Apply sideways thrust to aileron roll
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.AddForce(Vector3.left, ForceMode.Impulse);
+        }
+
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.AddForce(Vector3.right, ForceMode.Impulse);
+        }
+    }
 }
